79102911da6a50f57602013367c3ab2d6d3e91cf
rafha-sprada <rah.sprada@gmail.com>
2025-09-22 18:37:45 -0300

    Create test_app_funcs.py

---
+++ tests/test_app_funcs.py
@@ -0,0 +1,22 @@
+
+import pytest
+from app_funcs import soma, subtracao, multiplicacao, divisao, saudacao
+
+def test_soma():
+    assert soma(2, 3) == 5
+
+def test_subtracao():
+    assert subtracao(10, 4) == 6
+
+def test_multiplicacao():
+    assert multiplicacao(3, 5) == 15
+
+def test_divisao_normal():
+    assert divisao(10, 2) == 5
+
+def test_divisao_erro_zero():
+    with pytest.raises(ValueError):
+        divisao(5, 0)
+
+def test_saudacao():
+    assert saudacao("Rui") == "Olá, Rui!"
